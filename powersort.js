8f7bb2a36c6108cee28171319c2fffb1b96ca6f1
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-08-30 20:01:52 +0000

    We can sort using two criteria.

---
+++ powersort.js
@@ -0,0 +1,26 @@
+var students = [
+    { id: 1, name: "bruce",    age: 40 },
+    { id: 2, name: "zoidberg", age: 22 },
+    { id: 3, name: "alex",     age: 22 },
+    { id: 4, name: "alex",     age: 30 }
+  ];
+
+function powersort(a, b){
+    if(a.name === b.name) {
+        return b.age - a.age;
+    }
+
+    if(a.name < b.name) {
+        return -1;
+    }
+
+    if(a.name > b.name) {
+        return 1;
+    }
+
+    return 0;
+}  
+
+var result = students.sort(powersort);
+
+console.log(result);
